4d1b6c264ebeead6ab93ce8638cdc52d0ace1c96
Maxime Segot <max.segot@gmail.com>
2023-01-16 15:51:30 +0100

    Ajout des besoin de création de partie et verification de connexion

--- monostreet/createGame.php
+++ monostreet/createGame.php
@@ -1,13 +1,18 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['pseudo'])) {
+    header('Location: connexion.php');
+}
 
 
 ?>
 
-<form method="POST" action="" align="center">
-    <input type="text" name="pseudo" class="input">
+<form method="POST" action="" text-align="center">
+    <input type="text" name="pseudo" class="input" value="Votre Pseudo">
     </br>
-    <input type="text" name="mdp" class="input">
+    <input type="number" name="nbJoueur" class="input" min="2" max="4">
+    </br>
+    <input type="text" name="mdp" class="input" value="Mot De Passe de la partie :">
     </br>
     <input type="submit" name="envoie">
 </form>
