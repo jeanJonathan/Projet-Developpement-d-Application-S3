ec1c99fa1b3c13c3700813154093e7c224afe41b
Maxime Segot <max.segot@gmail.com>
2023-01-16 16:12:06 +0100

    Rectificatif du html de createGame.php

--- monostreet/createGame.php
+++ monostreet/createGame.php
@@ -8,11 +8,9 @@ if (!isset($_SESSION['pseudo'])) {
 ?>
 
 <form method="POST" action="" text-align="center">
-    <input type="text" name="pseudo" class="input" value="Votre Pseudo">
+    Votre pseudo:<input type="text" name="pseudo" class="input">
     </br>
-    <input type="number" name="nbJoueur" class="input" min="2" max="4">
-    </br>
-    <input type="text" name="mdp" class="input" value="Mot De Passe de la partie :">
+    Nombre de joueur<input type="number" name="nbJoueur" class="input" min="2" max="4">
     </br>
     <input type="submit" name="envoie">
 </form>
